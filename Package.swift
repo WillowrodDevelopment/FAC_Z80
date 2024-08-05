47359bd920845095f2772556435da3a9af3ab831
mikehallCD <mike@chipmunkdata.co.uk>
2024-08-05 16:14:45 +0100

    Update Package

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        //.package(url: "https://github.com/Willowrod/FAC_Common.git", from: "0.1.1")
-        .package(path: "../FAC_Common")
+        .package(url: "https://github.com/Willowrod/FAC_Common.git", from: "1.0.0")
+        //.package(path: "../FAC_Common")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
